40882711b168ad79c1ac4c5b773460e3f96bd7a3
Niels Vanhasbroeck <niels.vanhasbroeck@gmail.com>
2025-07-09 09:40:05 +0200

    Introduce tests for determinstic and stochastic simulations

--- tests/testthat/test_simulate.R
+++ tests/testthat/test_simulate.R
@@ -171,3 +171,204 @@ testthat::test_that(
         testthat::expect_equal(tst, ref)
     }
 )
+
+testthat::test_that(
+    "Check deterministic content for the simulation of the models",
+    {
+        X <- 1:10
+        Z <- 1:10
+
+        # Linear model
+        model <- paramrel::linear(1:2, sd = 0)
+
+        tst <- paramrel::simulate(model, X = X)
+        ref <- data.frame(
+            time = 1:10,
+            y = 1 + 2 * 1:10,
+            x = 1:10,
+            z = 0
+        )
+
+        testthat::expect_equal(tst, ref)
+
+
+
+        # Quadratic model
+        model <- paramrel::quadratic(1:3, sd = 0)
+
+        tst <- paramrel::simulate(model, X = X)
+        ref <- data.frame(
+            time = 1:10,
+            y = 1 + 2 * 1:10 + 3 * (1:10)^2,
+            x = 1:10,
+            z = 0
+        )
+
+        testthat::expect_equal(tst, ref)
+
+
+
+        # Cubic model
+        model <- paramrel::cubic(1:4, sd = 0)
+
+        tst <- paramrel::simulate(model, X = X)
+        ref <- data.frame(
+            time = 1:10,
+            y = 1 + 2 * 1:10 + 3 * (1:10)^2 + 4 * (1:10)^3,
+            x = 1:10,
+            z = 0
+        )
+
+        testthat::expect_equal(tst, ref)
+
+
+
+        # Main effects model
+        model <- paramrel::main_effect(1:3, sd = 0)
+
+        tst <- paramrel::simulate(model, X = cbind(X, Z))
+        ref <- data.frame(
+            time = 1:10,
+            y = 1 + 2 * 1:10 + 3 * 1:10,
+            x = 1:10,
+            z = 1:10
+        )
+
+        testthat::expect_equal(tst, ref)
+
+
+
+        # Interaction model
+        model <- paramrel::interaction(1:4, sd = 0)
+
+        tst <- paramrel::simulate(model, X = cbind(X, Z))
+        ref <- data.frame(
+            time = 1:10,
+            y = 1 + 2 * 1:10 + 3 * 1:10 + 4 * 1:10 * 1:10,
+            x = 1:10,
+            z = 1:10
+        )
+
+        testthat::expect_equal(tst, ref)
+
+
+
+        # AR(1) model
+        model <- paramrel::ar1(c(1, 0.9), sd = 0)
+
+        tst <- paramrel::simulate(model, X = X)
+        ref <- data.frame(
+            time = 1:10,
+            y = rep(1/0.1, each = 10),
+            x = 1:10,
+            z = 0
+        )
+
+        testthat::expect_equal(tst, ref)
+
+
+
+        # Commented out: Most difficult model to test deterministically, needs
+        # significantly more code
+        #
+        # # ARX model
+        # model <- paramrel::arx(c(1, 0.9, 2), sd = 0)
+
+        # tst <- paramrel::simulate(model, X = X)
+        # ref <- data.frame(
+        #     time = 1:10,
+        #     y = rep(1/0.1, each = 10),
+        #     x = 1:10,
+        #     z = 0
+        # )
+
+        # testthat::expect_equal(tst, ref)
+    }        
+)
+
+testthat::test_that(
+    "Check stochastic content for the simulation of the models",
+    {
+        X <- rep(1, 100000)
+        Z <- rep(1, 100000)
+
+        # Linear model
+        model <- paramrel::linear(1:2, sd = 1)
+        tst <- paramrel::simulate(model, X = X)
+        
+        testthat::expect_equal(mean(tst$y), sum(1:2), tolerance = 1e-2)
+        testthat::expect_equal(sd(tst$y), 1, tolerance = 1e-2)
+
+
+
+        # Quadratic model
+        model <- paramrel::quadratic(1:3, sd = 1)
+        tst <- paramrel::simulate(model, X = X)
+        
+        testthat::expect_equal(mean(tst$y), sum(1:3), tolerance = 1e-2)
+        testthat::expect_equal(sd(tst$y), 1, tolerance = 1e-2)
+
+
+
+        # Cubic model
+        model <- paramrel::cubic(1:4, sd = 1)
+        tst <- paramrel::simulate(model, X = X)
+        
+        testthat::expect_equal(mean(tst$y), sum(1:4), tolerance = 1e-2)
+        testthat::expect_equal(sd(tst$y), 1, tolerance = 1e-2)
+
+
+
+        # Main effects model
+        model <- paramrel::main_effect(1:3, sd = 1)
+        tst <- paramrel::simulate(model, X = cbind(X, Z))
+        
+        testthat::expect_equal(mean(tst$y), sum(1:3), tolerance = 1e-2)
+        testthat::expect_equal(sd(tst$y), 1, tolerance = 1e-2)
+
+
+
+        # Interaction model
+        model <- paramrel::interaction(1:4, sd = 1)
+        tst <- paramrel::simulate(model, X = cbind(X, Z))
+        
+        testthat::expect_equal(mean(tst$y), sum(1:4), tolerance = 1e-2)
+        testthat::expect_equal(sd(tst$y), 1, tolerance = 1e-2)
+
+
+
+        # Commented out: Most difficult model to test deterministically, needs
+        # significantly more code
+        #
+        # # AR(1) model
+        # model <- paramrel::ar1(c(1, 0.9), sd = 0)
+
+        # tst <- paramrel::simulate(model, X = X)
+        # ref <- data.frame(
+        #     time = 1:10,
+        #     y = rep(1/0.1, each = 10),
+        #     x = 1:10,
+        #     z = 0
+        # )
+
+        # testthat::expect_equal(tst, ref)
+
+
+
+        # Commented out: Most difficult model to test deterministically, needs
+        # significantly more code
+        #
+        # # ARX model
+        # model <- paramrel::arx(c(1, 0.9, 2), sd = 0)
+
+        # tst <- paramrel::simulate(model, X = X)
+        # ref <- data.frame(
+        #     time = 1:10,
+        #     y = rep(1/0.1, each = 10),
+        #     x = 1:10,
+        #     z = 0
+        # )
+
+        # testthat::expect_equal(tst, ref)
+    }        
+)
